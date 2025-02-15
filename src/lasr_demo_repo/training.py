938dd90219af4a5052bf1efaee5c14b163a976c3
UFO-101 <josephmiller101@gmail.com>
2025-02-15 18:48:46 +0000

    Add VSCode settings and some dummy code.

---
+++ src/lasr_demo_repo/training.py
@@ -0,0 +1,9 @@
+def add_ten_numbers_together(
+    a: int, b: int, c: int, d: int, e: int, f: int, g: int, h: int, i: int, j: int
+) -> int:
+    for i in range(10):
+        print(i)
+    return a + b + c + d + e + f + g + h + i + j
+
+
+print(add_ten_numbers_together(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
